18c9801cd48ed4a1e2de5b83220a69494781b4bd
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-22 21:53:33 +0100

    Remove unused per-entity annotation storage

--- include/cmm/detail/entities/entity.hpp
+++ include/cmm/detail/entities/entity.hpp
@@ -2,8 +2,6 @@
 #define CALLMEMAYBE_ENTITY_HPP
 
 #include <string_view>
-#include <vector>
-#include "cmm/info.hpp"
 
 namespace cmm {
 namespace detail {
@@ -20,13 +18,9 @@ public:
         display_name_ = display_name;
     }
 
-    void add_annotation(cmm::info id) { annotations_.push_back(id); }
-    const std::vector<cmm::info>& annotations() const { return annotations_; }
-
 protected:
     std::string_view name_;
     std::string_view display_name_;
-    std::vector<cmm::info> annotations_;
 };
 
 } // namespace detail
