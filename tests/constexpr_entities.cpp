95e10dfe542c12d5e19f6ab67cfa0fc439334d0f
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-23 20:25:13 +0100

    consteval registry: cover leaf metadata in static assertions

--- tests/constexpr_entities.cpp
+++ tests/constexpr_entities.cpp
@@ -1,9 +1,12 @@
 #include <cstdint>
 
+#include "cmm/detail/entities/base.hpp"
+#include "cmm/detail/entities/data_member.hpp"
 #include "cmm/detail/entities/entity.hpp"
 #include "cmm/detail/entities/enumerator.hpp"
 #include "cmm/detail/entities/parameter.hpp"
 #include "cmm/detail/entities/type.hpp"
+#include "cmm/detail/entities/variable.hpp"
 
 constexpr bool constexpr_metadata_roundtrip()
 {
@@ -35,6 +38,29 @@ constexpr bool constexpr_metadata_roundtrip()
     enumerator.set_parent_id(44);
     if (enumerator.value_bits() != UINT64_C(0x8000000000000000)) return false;
     if (enumerator.is_signed() || enumerator.parent_id() != 44) return false;
+
+    cmm::detail::DataMember data_member("field");
+    data_member.set_type_id(55);
+    data_member.set_parent_id(66);
+    data_member.set_offset_bytes(8);
+    data_member.set_offset_bits(64);
+    data_member.set_is_const(true);
+    data_member.set_is_bit_field(false);
+    if (data_member.type_id() != 55 || data_member.parent_id() != 66) return false;
+    if (data_member.offset_bytes() != 8 || data_member.offset_bits() != 64) return false;
+    if (!data_member.is_const() || data_member.is_static() || data_member.is_bit_field()) return false;
+
+    cmm::detail::Variable variable("global", 77);
+    variable.set_is_const(true);
+    if (variable.type_id() != 77 || !variable.is_const()) return false;
+    if (variable.address() != nullptr || variable.mutable_address() != nullptr) return false;
+
+    cmm::detail::Base base("Base", 88, 99);
+    base.set_access(cmm::detail::Access::Protected);
+    base.set_is_virtual(true);
+    if (base.type_id() != 88 || base.parent_id() != 99) return false;
+    if (base.access() != cmm::detail::Access::Protected || !base.is_virtual()) return false;
+    if (base.is_runtime_accessible()) return false;
 
     return true;
 }
