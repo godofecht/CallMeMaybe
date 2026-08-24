f43e1c16c53484253534ad699939c8dac9203cd7
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-24 14:55:24 +0100

    flow bridge: track owned object destruction

--- bridge/flow/e2e_fixture.hpp
+++ bridge/flow/e2e_fixture.hpp
@@ -12,11 +12,18 @@ enum class CmmE2eCode : std::uint64_t {
     High = UINT64_C(0x8000000000000000)
 };
 
+inline int cmm_e2e_destroyed_count = 0;
+
 class CmmE2eCounter {
 public:
     [[=cmm::reflectable]] explicit CmmE2eCounter(int value)
         : value_(value)
     {
+    }
+
+    ~CmmE2eCounter()
+    {
+        ++cmm_e2e_destroyed_count;
     }
 
     [[=cmm::reflectable]] int add(int amount)
