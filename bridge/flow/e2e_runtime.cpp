267fceb3d1c941653d5632676cba83bfa53ea410
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-24 13:59:09 +0100

    flow bridge: register reflected class fixture

--- bridge/flow/e2e_runtime.cpp
+++ bridge/flow/e2e_runtime.cpp
@@ -5,6 +5,7 @@
 
 extern "C" std::int32_t cmm_flow_e2e_register()
 {
-    return static_cast<std::int32_t>(
-        cmm::flow::register_bindings<^^cmm_e2e_add, ^^cmm_e2e_scale, ^^cmm_e2e_not, ^^cmm_e2e_echo, ^^cmm_e2e_byte_sum, ^^cmm_e2e_bytes, ^^cmm_e2e_pointer, ^^cmm_e2e_mut_ref, ^^cmm_e2e_const_ref, ^^cmm_e2e_mut_ref_f64, ^^cmm_e2e_const_ref_bool, ^^cmm_e2e_pointer_u64, ^^cmm_e2e_mut_ref_u64, ^^cmm_e2e_enum_flip>());
+    cmm::Error error = cmm::flow::register_bindings<^^cmm_e2e_add, ^^cmm_e2e_scale, ^^cmm_e2e_not, ^^cmm_e2e_echo, ^^cmm_e2e_byte_sum, ^^cmm_e2e_bytes, ^^cmm_e2e_pointer, ^^cmm_e2e_mut_ref, ^^cmm_e2e_const_ref, ^^cmm_e2e_mut_ref_f64, ^^cmm_e2e_const_ref_bool, ^^cmm_e2e_pointer_u64, ^^cmm_e2e_mut_ref_u64, ^^cmm_e2e_enum_flip>();
+    if (error == cmm::Error::Success) error = cmm::register_rrefl<^^CmmE2eCounter>();
+    return static_cast<std::int32_t>(error);
 }
