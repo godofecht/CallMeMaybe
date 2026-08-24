d5fe6b32180447ccd6dccde8db9d97eb456e22b8
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-24 12:45:39 +0100

    flow bridge: materialize enum signature dependencies

--- bridge/flow/generator.hpp
+++ bridge/flow/generator.hpp
@@ -18,21 +18,42 @@ struct GenerationResult {
 std::string wrapper_name(cmm::info function_id);
 GenerationResult generate_wrapper_fragment(std::span<const cmm::info> function_ids);
 
+namespace detail {
+
+template <std::meta::info TypeRefl>
+void register_enum_dependency(cmm::Error& error)
+{
+    if (error != cmm::Error::Success) return;
+    constexpr std::meta::info decayed = std::meta::remove_cvref(TypeRefl);
+    if constexpr (std::meta::is_enum_type(decayed)) error = cmm::register_rrefl<decayed>();
+}
+
+template <std::meta::info Function>
+void register_binding(cmm::Error& error)
+{
+    if (error != cmm::Error::Success) return;
+
+    if constexpr (!std::meta::is_constructor(Function) && !std::meta::is_destructor(Function))
+    {
+        register_enum_dependency<std::meta::return_type_of(Function)>(error);
+    }
+
+    template for (constexpr std::meta::info parameter : std::define_static_array(std::meta::parameters_of(Function)))
+    {
+        register_enum_dependency<std::meta::type_of(parameter)>(error);
+    }
+
+    if (error == cmm::Error::Success) error = cmm::register_rrefl<Function>();
+}
+
+} // namespace detail
+
 template <std::meta::info... Functions>
 GenerationResult generate_wrapper_fragment()
 {
     cmm::Error registration_error = cmm::Error::Success;
-
-    auto register_one = [&]<std::meta::info Function>() {
-        if (registration_error != cmm::Error::Success) return;
-        registration_error = cmm::register_rrefl<Function>();
-    };
-
-    (register_one.template operator()<Functions>(), ...);
-    if (registration_error != cmm::Error::Success)
-    {
-        return GenerationResult{registration_error, {}};
-    }
+    (detail::register_binding<Functions>(registration_error), ...);
+    if (registration_error != cmm::Error::Success) return GenerationResult{registration_error, {}};
 
     const std::array<cmm::info, sizeof...(Functions)> ids{cmm::get_id<Functions>()...};
     return generate_wrapper_fragment(ids);
@@ -42,13 +63,7 @@ template <std::meta::info... Functions>
 cmm::Error register_bindings()
 {
     cmm::Error error = cmm::Error::Success;
-
-    auto register_one = [&]<std::meta::info Function>() {
-        if (error != cmm::Error::Success) return;
-        error = cmm::register_rrefl<Function>();
-    };
-
-    (register_one.template operator()<Functions>(), ...);
+    (detail::register_binding<Functions>(error), ...);
     return error;
 }
 
