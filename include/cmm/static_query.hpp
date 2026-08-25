19cc667bf28f514c43b1a878086ca1adcbb8c568
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-25 15:44:12 +0100

    research: expose static type predicates

--- include/cmm/static_query.hpp
+++ include/cmm/static_query.hpp
@@ -90,6 +90,44 @@ inline bool is_enumerator(cmm::info i)
     return detail::active_static_registry().try_get_as<detail::Enumerator>(i) != nullptr;
 }
 
+#define CMM_DEFINE_STATIC_TYPE_PREDICATE(name, flag)                   \
+    inline bool name(cmm::info i)                                      \
+    {                                                                  \
+        if (!detail::static_valid(i)) return false;                     \
+        return detail::visit_static_entity(i, [](const auto& arg)      \
+        {                                                              \
+            using T = std::decay_t<decltype(arg)>;                     \
+            if constexpr (std::is_base_of_v<detail::Type, T>)          \
+            {                                                          \
+                return arg.flags().flag;                               \
+            }                                                          \
+            return false;                                              \
+        });                                                            \
+    }
+
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_void_type,             is_void)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_null_pointer_type,     is_null_pointer)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_integral_type,         is_integral)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_floating_point_type,   is_floating_point)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_arithmetic_type,       is_arithmetic)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_fundamental_type,      is_fundamental)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_pointer_type,          is_pointer)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_lvalue_reference_type, is_lvalue_reference)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_rvalue_reference_type, is_rvalue_reference)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_reference_type,        is_reference)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_class_type,            is_class)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_union_type,            is_union)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_enum_type,             is_enum)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_scoped_enum_type,      is_scoped_enum)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_array_type,            is_array)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_function_type,         is_function_type)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_const_type,            is_const)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_volatile_type,         is_volatile)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_signed_type,           is_signed)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_unsigned_type,         is_unsigned)
+
+#undef CMM_DEFINE_STATIC_TYPE_PREDICATE
+
 } // namespace cmm
 
 #endif // CALLMEMAYBE_STATIC_QUERY_HPP
