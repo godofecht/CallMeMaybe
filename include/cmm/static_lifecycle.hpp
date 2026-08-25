4595c3c5f11f41d7013467c36029e4b86e72c380
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-25 20:44:44 +0100

    Port DynamicObject lifecycle to static registry

--- include/cmm/static_lifecycle.hpp
+++ include/cmm/static_lifecycle.hpp
@@ -3,9 +3,13 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdlib>
 #include <meta>
+#include <span>
 #include <type_traits>
+#include <utility>
 
+#include "cmm/static_invoke.hpp"
 #include "cmm/static_meta.hpp"
 #include "cmm/detail/entities/class.hpp"
 #include "cmm/detail/entities/function.hpp"
@@ -68,6 +72,88 @@ inline cmm::info get_destructor(cmm::info class_id)
 }
 
 } // namespace lookup
+
+class DynamicObject {
+public:
+    DynamicObject() = default;
+
+    DynamicObject(Value pointer, cmm::info destructor)
+        : pointer_(std::move(pointer)), destructor_(destructor) {}
+
+    DynamicObject(const DynamicObject&) = delete;
+    DynamicObject& operator=(const DynamicObject&) = delete;
+
+    DynamicObject(DynamicObject&& other) noexcept
+        : pointer_(std::move(other.pointer_)), destructor_(other.destructor_)
+    {
+        other.destructor_ = cmm::invalid_info;
+    }
+
+    DynamicObject& operator=(DynamicObject&& other) noexcept
+    {
+        if (this != &other)
+        {
+            destroy();
+            pointer_ = std::move(other.pointer_);
+            destructor_ = other.destructor_;
+            other.destructor_ = cmm::invalid_info;
+        }
+        return *this;
+    }
+
+    ~DynamicObject()
+    {
+        destroy();
+    }
+
+    explicit operator bool() const
+    {
+        return pointer_.has_value();
+    }
+
+    template <typename T>
+    T* get()
+    {
+        if (!pointer_.has_value()) return nullptr;
+        return pointer_.template get<T*>();
+    }
+
+    template <typename T>
+    T* release()
+    {
+        T* result = get<T>();
+        pointer_ = Value{};
+        destructor_ = cmm::invalid_info;
+        return result;
+    }
+
+private:
+    void destroy() noexcept
+    {
+        if (!pointer_.has_value() || destructor_ == cmm::invalid_info) return;
+
+        Value ignored;
+        std::span<Value> args(&pointer_, 1);
+        if (reflect_invoke(destructor_, args, ignored) != cmm::Error::Success) std::abort();
+        pointer_ = Value{};
+        destructor_ = cmm::invalid_info;
+    }
+
+    Value pointer_;
+    cmm::info destructor_{cmm::invalid_info};
+};
+
+template <typename... Args>
+DynamicObject construct(cmm::info class_id, Args&&... args)
+{
+    const cmm::info constructor = lookup::get_constructor<Args...>(class_id);
+    const cmm::info destructor = lookup::get_destructor(class_id);
+    if (constructor == cmm::invalid_info || destructor == cmm::invalid_info) std::abort();
+
+    Value pointer = invoke(constructor, std::forward<Args>(args)...);
+    return DynamicObject(std::move(pointer), destructor);
+}
+
 } // namespace cmm
 
 #endif // CALLMEMAYBE_STATIC_LIFECYCLE_HPP
