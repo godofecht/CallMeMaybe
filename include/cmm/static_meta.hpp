934a6d71c93c3692920cf069e0d89bea2dc5d04f
Abhishek Shivakumar <abhishek.shivakumar@gmail.com>
2026-08-26 09:43:01 +0100

    research: expand static metadata query parity

--- include/cmm/static_meta.hpp
+++ include/cmm/static_meta.hpp
@@ -127,6 +127,19 @@ inline std::vector<cmm::info> nonstatic_data_members_of(cmm::info i)
     return {view.begin(), view.end()};
 }
 
+inline std::span<const cmm::info> static_data_members_view_of(cmm::info i)
+{
+    if (!detail::static_valid(i)) return {};
+    const auto* cls = detail::active_static_registry().try_get_as<detail::Class>(i);
+    return cls ? cls->static_data_members() : std::span<const cmm::info>{};
+}
+
+inline std::vector<cmm::info> static_data_members_of(cmm::info i)
+{
+    const auto view = static_data_members_view_of(i);
+    return {view.begin(), view.end()};
+}
+
 inline std::span<const cmm::info> bases_view_of(cmm::info i)
 {
     if (!detail::static_valid(i)) return {};
@@ -190,6 +203,20 @@ inline std::vector<cmm::info> enumerators_of(cmm::info i)
     return result;
 }
 
+inline const void* const_address_of(cmm::info i)
+{
+    if (!detail::static_valid(i)) return nullptr;
+    const auto* variable = detail::active_static_registry().try_get_as<detail::Variable>(i);
+    return variable ? variable->address() : nullptr;
+}
+
+inline void* address_of(cmm::info i)
+{
+    if (!detail::static_valid(i)) return nullptr;
+    const auto* variable = detail::active_static_registry().try_get_as<detail::Variable>(i);
+    return variable ? variable->mutable_address() : nullptr;
+}
+
 inline cmm::Error try_value_of(cmm::info i, std::int64_t& out)
 {
     if (!detail::static_valid(i)) return cmm::Error::EntityNotFound;
@@ -217,6 +244,13 @@ inline std::vector<cmm::info> parameters_of(cmm::info i)
 {
     const auto view = parameters_view_of(i);
     return {view.begin(), view.end()};
+}
+
+inline cmm::info return_type_of(cmm::info i)
+{
+    if (!detail::static_valid(i)) return invalid_info;
+    const auto* function = detail::active_static_registry().try_get_as<detail::Function>(i);
+    return function ? function->return_type_id() : invalid_info;
 }
 
 inline cmm::Error reflect_invoke(cmm::info target, std::span<Value> args, Value& out)
@@ -286,6 +320,113 @@ inline std::size_t offset_of(cmm::info i)
     (void)try_offset_of(i, value);
     return value;
 }
+
+inline std::size_t bit_offset_of(cmm::info i)
+{
+    if (!detail::static_valid(i)) return 0;
+    const auto* member = detail::active_static_registry().try_get_as<detail::DataMember>(i);
+    return member ? static_cast<std::size_t>(member->offset_bits()) : 0;
+}
+
+inline bool is_function(cmm::info i)
+{
+    return detail::static_valid(i) &&
+           detail::active_static_registry().try_get_as<detail::Function>(i) != nullptr;
+}
+
+inline bool is_constructor(cmm::info i)
+{
+    const auto* function = detail::static_valid(i)
+        ? detail::active_static_registry().try_get_as<detail::Function>(i)
+        : nullptr;
+    return function && function->is_constructor();
+}
+
+inline bool is_destructor(cmm::info i)
+{
+    const auto* function = detail::static_valid(i)
+        ? detail::active_static_registry().try_get_as<detail::Function>(i)
+        : nullptr;
+    return function && function->is_destructor();
+}
+
+inline bool is_const_member_function(cmm::info i)
+{
+    const auto* function = detail::static_valid(i)
+        ? detail::active_static_registry().try_get_as<detail::Function>(i)
+        : nullptr;
+    return function && function->is_const_member_function();
+}
+
+inline bool is_static_member(cmm::info i)
+{
+    if (!detail::static_valid(i)) return false;
+    return detail::visit_static_entity(i, [](const auto& arg) -> bool
+    {
+        using T = std::decay_t<decltype(arg)>;
+        if constexpr (std::is_same_v<T, detail::Function>) return arg.is_static_function();
+        if constexpr (std::is_same_v<T, detail::DataMember>) return arg.is_static();
+        return false;
+    });
+}
+
+inline bool is_nonstatic_data_member(cmm::info i)
+{
+    if (!detail::static_valid(i)) return false;
+    const auto* member = detail::active_static_registry().try_get_as<detail::DataMember>(i);
+    return member && !member->is_static();
+}
+
+inline bool is_const_data_member(cmm::info i)
+{
+    if (!detail::static_valid(i)) return false;
+    const auto* member = detail::active_static_registry().try_get_as<detail::DataMember>(i);
+    return member && member->is_const();
+}
+
+inline bool is_enumerator(cmm::info i)
+{
+    return detail::static_valid(i) &&
+           detail::active_static_registry().try_get_as<detail::Enumerator>(i) != nullptr;
+}
+
+#define CMM_DEFINE_STATIC_TYPE_PREDICATE(name, flag)                  \
+    inline bool name(cmm::info i)                                     \
+    {                                                                 \
+        if (!detail::static_valid(i)) return false;                   \
+        return detail::visit_static_entity(i, [](const auto& arg) -> bool \
+        {                                                             \
+            using T = std::decay_t<decltype(arg)>;                    \
+            if constexpr (std::is_base_of_v<detail::Type, T>)        \
+            {                                                         \
+                return arg.flags().flag;                              \
+            }                                                         \
+            return false;                                             \
+        });                                                           \
+    }
+
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_void_type,             is_void)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_null_pointer_type,     is_null_pointer)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_integral_type,         is_integral)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_floating_point_type,   is_floating_point)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_arithmetic_type,       is_arithmetic)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_fundamental_type,      is_fundamental)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_pointer_type,          is_pointer)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_lvalue_reference_type, is_lvalue_reference)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_rvalue_reference_type, is_rvalue_reference)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_reference_type,        is_reference)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_class_type,            is_class)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_union_type,            is_union)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_enum_type,             is_enum)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_scoped_enum_type,      is_scoped_enum)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_array_type,            is_array)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_function_type,         is_function_type)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_const_type,            is_const)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_volatile_type,         is_volatile)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_signed_type,           is_signed)
+CMM_DEFINE_STATIC_TYPE_PREDICATE(is_unsigned_type,         is_unsigned)
+
+#undef CMM_DEFINE_STATIC_TYPE_PREDICATE
 
 namespace lookup {
 
